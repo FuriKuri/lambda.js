42d6f7e4778597ef95a580c541b9f96ebbc1ce82
Theo Pack <tf.pack@gmail.com>
2014-11-09 09:22:41 +0100

    Add reduce function

---
+++ out/lib/lambda.js
@@ -0,0 +1,26 @@
+System.register("../../lib/lambda", [], function() {
+  "use strict";
+  var __moduleName = "../../lib/lambda";
+  function require(path) {
+    return $traceurRuntime.require("../../lib/lambda", path);
+  }
+  var defineFunction = require('./function');
+  function Lambda(binder, body) {
+    if (false === (this instanceof Lambda)) {
+      console.log('Warning: Lambda constructor called without "new" operator');
+      return new Lambda(binder, body);
+    }
+    this.binder = binder;
+    this.body = body;
+    return Object.freeze(this);
+  }
+  module.exports = Lambda;
+  defineFunction(Lambda.prototype, "toString", function() {
+    return 'λ ' + this.binder + ' → ' + this.body.toString();
+  });
+  defineFunction(Lambda.prototype, "reduce", function() {
+    return new Lambda(this.binder, this.body.reduce());
+  });
+  return {};
+});
+System.get("../../lib/lambda" + '');
