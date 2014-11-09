ba61b068b2c7f507c3ee173e4858bfae1dfec925
Theo Pack <tf.pack@gmail.com>
2014-11-09 09:49:35 +0100

    Add reduce function

---
+++ out/lib/application.js
@@ -0,0 +1,44 @@
+System.register("../../lib/application", [], function() {
+  "use strict";
+  var __moduleName = "../../lib/application";
+  function require(path) {
+    return $traceurRuntime.require("../../lib/application", path);
+  }
+  var defineFunction = require('./function');
+  var Lambda = require('./lambda');
+  function Application(left, right) {
+    if (false === (this instanceof Application)) {
+      console.log('Warning: Application constructor called without "new" operator');
+      return new Application(left, right);
+    }
+    this.left = left;
+    this.right = right;
+    return Object.freeze(this);
+  }
+  module.exports = Application;
+  defineFunction(Application.prototype, "toString", function() {
+    var l = this.left.toString();
+    var r = this.right.toString();
+    if (this.left instanceof Lambda) {
+      l = "(" + l + ")";
+    }
+    if (this.right instanceof Lambda || this.right instanceof Application) {
+      r = "(" + r + ")";
+    }
+    return l + " " + r;
+  });
+  defineFunction(Application.prototype, "reduce", function() {
+    var nr = this.right.reduce();
+    var nl = this.left.reduce();
+    if (nl instanceof Lambda) {
+      var r = nl.body.substitude(nl.binder, nr);
+      console.log(new Application(nl, nr).toString() + " → " + r.toString());
+      var s = r.reduce();
+      return s;
+    } else {
+      return new Application(nl, nr);
+    }
+  });
+  return {};
+});
+System.get("../../lib/application" + '');
