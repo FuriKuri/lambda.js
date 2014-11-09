c2f4bf4f6760f75b49dba79f37ec3ea7f8207612
Theo Pack <tf.pack@gmail.com>
2014-11-09 10:16:35 +0100

    Add substitute function

--- out/lib/lambda.js
+++ out/lib/lambda.js
@@ -5,6 +5,7 @@ System.register("../../lib/lambda", [], function() {
     return $traceurRuntime.require("../../lib/lambda", path);
   }
   var defineFunction = require('./function');
+  var Variable = require('./variable');
   function Lambda(binder, body) {
     if (false === (this instanceof Lambda)) {
       console.log('Warning: Lambda constructor called without "new" operator');
@@ -20,6 +21,21 @@ System.register("../../lib/lambda", [], function() {
   });
   defineFunction(Lambda.prototype, "reduce", function() {
     return new Lambda(this.binder, this.body.reduce());
+  });
+  var gensymcount = 0;
+  function gensym() {
+    gensymcount = gensymcount + 1;
+    return '#x' + gensymcount;
+  }
+  defineFunction(Lambda.prototype, "substitute", function(o, n) {
+    if (this.binder === o) {
+      return this;
+    } else if ((this.body.free().indexOf(n.name) !== -1) && (n.free().indexOf(this.binder) !== -1)) {
+      var ns = gensym();
+      return new Lambda(ns, this.body.substitute(this.binder, new Variable(ns)).substitute(o, n));
+    } else {
+      return new Lambda(this.binder, this.body.substitute(o, n));
+    }
   });
   return {};
 });
